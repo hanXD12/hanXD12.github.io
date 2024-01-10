fd4848dc16c21e6c486b8c7862766a24806bb64a
hanXD12 <156153708+hanXD12@users.noreply.github.com>
2024-01-10 12:50:13 +0700

    Add files via upload

---
+++ js/scipt.js
@@ -0,0 +1,18 @@
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+    
+};
+
+//klik luar side bar//
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
